c0b15cc656e2e7c730d5c498a822ad6805a549da
jerrychiu1 <JerryChiu1@hotmail.com>
2018-03-03 16:36:59 -0800

    Fix port for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://jerrychiu:hell0world@ds155278.mlab.com:55278/jchiu-todoapp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ToDoApp');
 
 module.exports = {mongoose};
