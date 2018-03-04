8670927e9c5b40ccaf35fc9a277724aa8cdf75a4
jerrychiu1 <JerryChiu1@hotmail.com>
2018-03-03 17:24:43 -0800

    Add delete /todos route

--- server/server.js
+++ server/server.js
@@ -54,6 +54,25 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send({});
+    }
+
+    Todo.findByIdAndRemove(id).then((todos) => {
+        if(!todos) {
+            return res.status(404).send();
+        }
+
+        res.send({todos});
+    }).catch((e) => {
+        res.status(404).send({});
+    })
+
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
